f2ee6c0d5bd558617c124f06c53f3b783b445509
Hércules Lopes <69056516+herculeslopes@users.noreply.github.com>
2023-11-14 12:49:42 -0300

    sidebar update

--- src/Components/Shape.tsx
+++ src/Components/Shape.tsx
@@ -1,13 +1,54 @@
 import styled from "styled-components";
+import { FaShapes } from "react-icons/fa";
 
-const StyledShape = styled.button`
+const StyledShape = styled.div`
+  display: flex;
+  gap: 10px;
+  /* justify-content: space-between; */
+  /* align-content: flex-start;  */
+
+  width: 30px;
+  height: 30px;
+  /* aspect-ratio: 1/1; */
+
+  padding: 5px;
+  border-radius: 5px;
   background-color: #a8a8a8;
+
+  /* transition: width 30s linear; */
+  transition: background-color .5s linear;
+  overflow: none;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #8d8d8d;
+  }
+  
+  &.is-expanded {
+    width: 100%;
+    background-color: #C9C9C9;
+    border-bottom: 1px solid black;
+    border-radius: 0;
+    /* width: fit-content; */
+    
+    &:hover {
+      transition: background-color .1s linear;
+      background-color: #8d8d8d;
+    }
+  }
+  
+  .react-icon {
+    /* margin: auto; */
+    width: 20px;
+    height: 20px;
+  }
 `
 
-const Shape = () => {
+const Shape = ({ shape, isExpanded }: { shape: string, isExpanded: boolean }) => {
   return <>
-    <StyledShape>
-      afjklsç
+    <StyledShape className={isExpanded ? 'is-expanded' : ''}>
+      <FaShapes className='react-icon' />
+      {isExpanded ? <span>{shape}</span> : <></>}
     </StyledShape>
   </>
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,15 +3,15 @@ import Sidebar from "../Components/Sidebar";
 import Menu from "../Components/Menu";
 
 const StyledHome = styled.main`
+  height: 100vh;
   display: flex;
-  background-color: red;
+  /* background-color: red; */
 `
 
 const Home = () => {
   return <>
     <StyledHome>
       <Sidebar />
-      
       <Menu />
     </StyledHome>
   </>
